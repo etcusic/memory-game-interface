a7c32f7c1174cb6ea186869d140b08ea8a7ec06e
Eli Cusic <tntitans31@gmail.com>
2020-11-29 11:35:28 -0600

    set up JS quiz cards and practice content to propagate

---
+++ scripts/quiz-page.js
@@ -0,0 +1,36 @@
+const practiceArray = [
+    "card1",
+    "card2",
+    "card3",
+    "card4",
+    "card5",
+    "card6",
+    "card7",
+    "card8",
+    "card9",
+    "card10",
+    "card11",
+    "card12",
+    "card13",
+    "card14",
+    "card15",
+    "card16",
+    "card17",
+    "card18",
+    "card19",
+    "card20ß"
+]
+
+const cardWrapper = function(card){
+    const newDiv = document.createElement('div')
+    newDiv.setAttribute('class', 'col s3')
+    newDiv.appendChild(card)
+    return newDiv
+}
+
+const quizCard = function(content){
+    const newDiv = document.createElement('div')
+    newDiv.setAttribute('class', 'card quiz-cards')
+    newDiv.innerText = content
+    return newDiv
+}
