edeb38ea7296430ac129d4914df424d4b478891a
Eli Cusic <tntitans31@gmail.com>
2020-11-30 13:22:23 -0600

    cleaned up JS code and added an alternate header for html page

--- scripts/create-page.js
+++ scripts/create-page.js
@@ -2,52 +2,11 @@
 
 // create the ability to press tab and make a new input rather than propogate 20
 
-// const kermitWindowImg = function(){
-//     const img = document.createElement('img')
-//     img.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhoWqE2Ld_pZos1i3W31M8kamLtjbV75XWEg&usqp=CAU')
-//     return img
-// }
-
-// const kermitWindowDiv = function(){
-//     const newDiv = document.createElement('div')
-//     newDiv.setAttribute('class', 'card-image')
-//     newDiv.appendChild(kermitWindowImg())
-//     return newDiv
-// }
-
-// const kermitDestruction = function(){
-//     const newDiv = document.createElement('div')
-//     newDiv.setAttribute('class', 'card-content center-align baby-blue')
-//     newDiv.innerText = "Take your time. Kermit can plot your destruction while you work"
-//     return newDiv
-// }
-
-// const windowKermitCard = function(){
-//     const newDiv = document.createElement('div')
-//     newDiv.setAttribute('class', 'card')
-//     newDiv.appendChild(kermitWindowDiv())
-//     newDiv.appendChild(kermitDestruction())
-//     return newDiv
-// }
-
-// const windowKermit = function(){
-//     const newDiv = document.createElement('div')
-//     newDiv.setAttribute('class', 'col s3 fiver')
-//     newDiv.appendChild(windowKermitCard())
-//     return newDiv
-// }
-
 const textInput = function(){
     const inputTag = document.createElement('input')
     inputTag.setAttribute('type', 'text')
     return inputTag
 }
-
-// const titleLabel = function(){
-//     const labelTag = document.createElement('label')
-//     labelTag.innerText = "Title"
-//     return labelTag
-// }
 
 const labelA = function(){
     const labelTag = document.createElement('label')
@@ -85,37 +44,6 @@ const inputWrapper = function(){
     return newDiv
 }
 
-// const inputTitle = function(){
-//     const newDiv = document.createElement('div')
-//     newDiv.setAttribute('class', 'input-field col s8 fiver')
-//     let input = textInput()
-//     input.setAttribute('id', 'title')
-//     newDiv.appendChild(input)
-//     newDiv.appendChild(titleLabel())
-//     return newDiv
-// }
-
-// const titleWrapper = function(){
-//     const newDiv = document.createElement('div')
-//     newDiv.setAttribute('id', 'deck-title')
-//     newDiv.setAttribute('class', 'row')
-//     newDiv.appendChild(inputTitle())
-//     return newDiv
-// }
-
-// const buttonTag = function(){
-//     const button = document.createElement('button')
-//     button.setAttribute('type', 'submit')
-//     button.setAttribute('class', 'btn-small waves-effect light-blue darken-4')
-//     button.innerText = 'Submit'
-//     return button
-// }
-
-// const buttonDiv = function(){
-//     const newDiv = document.createElement('div')
-//     newDiv.appendChild(buttonTag())
-//     return newDiv
-// }
 
 function lotsOfInputs(){
     const inputBlock = document.getElementById('input-block')
@@ -124,24 +52,3 @@ function lotsOfInputs(){
     }
 }
 
-// const formTag = function(){
-//     const form = document.createElement('form')
-//     form.setAttribute('id', 'deck-creator')
-//     form.appendChild(titleWrapper())
-//     lotsOfInputs(form)
-//     form.appendChild(buttonDiv())
-//     return form
-// }
-
-// const formWrapper = function(){
-//     const newDiv = document.createElement('div')
-//     newDiv.setAttribute('class', 'col s7 push-s1 fiver baby-blue')
-//     newDiv.appendChild(formTag())
-//     return newDiv
-// }
-
-// function setCreatorPage(){
-//     const mainDiv = document.getElementById('top-row')
-//     mainDiv.appendChild(windowKermit())
-//     mainDiv.appendChild(formWrapper())
-// }
--- scripts/executable.js
+++ scripts/executable.js
@@ -11,6 +11,8 @@ const one = document.getElementById('landing-page-wrapper')
 const two = document.getElementById('base-page-wrapper')
 const three = document.getElementById('create-page-wrapper')
 const four = document.getElementById('quiz-page-wrapper')
+const mainHeader = document.getElementById('main-header')
+const gameHeader = document.getElementById('game-header')
 
 function buttonOne(){
     document.querySelectorAll('.landing-page-button').forEach(item => {
@@ -19,6 +21,8 @@ function buttonOne(){
             two.style.display = "none"
             three.style.display = "none"
             four.style.display = "none"
+            mainHeader.style.display = "block"
+            gameHeader.style.display = "none"
         })
     })
 }
@@ -30,6 +34,8 @@ function buttonTwo(){
             two.style.display = "block"
             three.style.display = "none"
             four.style.display = "none"
+            mainHeader.style.display = "block"
+            gameHeader.style.display = "none"
         })
     })
 }
@@ -41,6 +47,8 @@ function buttonThree(){
             two.style.display = "none"
             three.style.display = "block"
             four.style.display = "none"
+            mainHeader.style.display = "block"
+            gameHeader.style.display = "none"
         })
     })
 }
@@ -52,6 +60,8 @@ function buttonFour(){
             two.style.display = "none"
             three.style.display = "none"
             four.style.display = "block"
+            mainHeader.style.display = "none"
+            gameHeader.style.display = "block"
         })
     })
 }
