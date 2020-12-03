defdb3ff4c3519c28a235c452cd2d171fa45f6fc
Eli Cusic <tntitans31@gmail.com>
2020-12-03 15:11:12 -0600

    Got fetch working to retrieve basic data :

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    loadAPI()
+    console.log("working")
+    loadDeck()
 
+})
+
+function loadDeck() {
+    fetch("http://localhost:3000/decks/1/cards")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
 }
