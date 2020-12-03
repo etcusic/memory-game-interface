a2b5df703b6b3e870e078535452e7b7c37bf5316
Eli Cusic <tntitans31@gmail.com>
2020-12-03 16:59:22 -0600

    set up cards being rendered onto the html page

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
 })
 
+function appendCard(card){
+    return `
+        <div class="quiz-card card col s2 light-blue darken-4 white-text center-align">
+            ${card.side_b}
+        </div>`
+}
+
+function displayCards(array){
+    const gameContainer = document.getElementById('game-container')
+    console.log(gameContainer)
+    array.forEach(element => {
+        gameContainer.innerHTML += appendCard(element)
+    });
+}
+
 function loadDeck() {
     fetch("http://localhost:3000/decks/1/cards")
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(data => displayCards(data))
 }
