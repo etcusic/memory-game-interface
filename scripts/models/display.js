5beb0d15524f9245c050b6e00459105e7a59b9f4
Eli Cusic <tntitans31@gmail.com>
2020-12-07 14:46:10 -0600

    created Display model for game interface and deleted Deck model

---
+++ scripts/models/display.js
@@ -0,0 +1,36 @@
+class Display {
+    
+    static nineCards () {
+        const wrapper = document.createElement('div')
+        wrapper.setAttribute('id', 'nine-cards')
+        this.row(1, wrapper)
+        this.row(4, wrapper)
+        this.row(7, wrapper)
+        return wrapper
+    }
+
+    static row (num, divNode) {
+        let newRow = document.createElement('div')
+        newRow.setAttribute('id', `quiz-row-${num}`)
+        newRow.setAttribute('class', 'row quiz-row')
+        this.cards(num, newRow)
+        divNode.appendChild(newRow)
+        // return newRow
+    }
+
+    static cards (num, row){
+        for (let i = 0; i < 3; i++){
+            let card = document.createElement('div')
+            card.setAttribute('id', `card-${i + num}`)
+            card.setAttribute('class', 'quizzers col s4')
+            row.appendChild(card)
+            // return card
+        }
+    }
+
+    static random () {
+        const div = document.createElement('div')
+        div.setAttribute('id', 'random')
+        return div
+    }
+}
