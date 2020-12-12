c0ab6f611a80de452e87544e22e1989e6767f48a
Eli Cusic <tntitans31@gmail.com>
2020-12-12 16:36:11 -0600

    built out Deck class and cleaned up Card class

--- scripts/models/card.js
+++ scripts/models/card.js
@@ -1,26 +1,17 @@
 class Card {
-    constructor(hash){
-        this.id = hash.id
-        this.side_a = hash.side_a
-        this.side_b = hash.side_b
-        // this.node = this.createNode()
-        // would it be better to create node here? how would I designate side A or B being shown??
+
+    constructor (id, side_a, side_b) {
+        this.id = id
+        this.sideA = side_a
+        this.sideB = side_b
+        this.position = 0
+        this.node = Card.blank()
     }
 
-    // add a param for if someone wants to decide on side A or side B
-    createNode(){
-        const cardNode = document.createElement('div')
-        cardNode.setAttribute('data-card', `${this.id}`)
-        cardNode.setAttribute('class', "quiz-card card center-align z-depth-4")
-        cardNode.setAttribute('value', this.side_a)
-        cardNode.innerText = this.side_b
-        return cardNode
-    }
-
-    static blank () {
-        const div = htmlNode.div()
-        div.setAttribute('class', 'quiz-card card center-align z-depth-4')
-        return div
-    }
+        static blank () {
+            let div = document.createElement('div')
+            div.setAttribute('class', 'quiz-card card center-align z-depth-4')
+            return div
+        }
 
 }
---
+++ scripts/models/deck.js
@@ -0,0 +1,41 @@
+class Deck {
+
+    constructor (id, title) {
+        this.id = id
+        this.title = title
+        this.cards = []
+    }
+
+    // setter function ??
+    createCards (cardsFromAPI) {
+        this.cards = cardsFromAPI.map( card => new Card (card.id, card.side_a, card.side_b))
+    }
+
+    static shuffle (arrayOfCards) {
+        return arrayOfCards.sort(() => Math.random() - 0.5)
+    }
+
+    setCardSides (side) {
+        switch (side) {
+        case 'A':
+            this.cards.map((card) => {
+                card.node.innerText = card.sideA 
+                card.node.value = card.sideB
+            })
+            break;
+        case 'B':
+            this.cards.map((card) => {
+                card.node.innerText = card.sideB 
+                card.node.value = card.sideA
+            })
+            break;
+        }
+    }
+
+    shuffleAndSet (side) {
+        Deck.shuffle(this.cards)
+        this.setCardSides(side)
+        return this.cards.map((card, index) => card.position = index + 1)
+    }
+
+}
