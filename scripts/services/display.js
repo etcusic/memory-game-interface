0011b5eaa55161c189758df9fd7c746bd07df16d
Eli Cusic <tntitans31@gmail.com>
2020-12-12 16:27:25 -0600

    built out Display class with to wiping and building pages

--- scripts/services/display.js
+++ scripts/services/display.js
@@ -1,20 +1,29 @@
-class DisplayInstance {
-    
-    constructor () {
-        this.panel = document.getElementById('left-container')
-        this.main = document.getElementById('game-container')
-    }
+class Display {
 
-    resetPanel() {
-        console.log(this)
-        // this.panel.children.each( node => node.remove() )
+    static wipePanel() {
         document.querySelectorAll('.panel').forEach( node => node.remove() )
     }
 
-    resetMain() {
-        console.log(this)
-        // this.main.children.forEach( node => node.remove() )
-        
+    static wipeMain() {
+        document.querySelectorAll('.main').forEach( node => node.remove() )
+    }
+
+    static wipeAll () {
+        this.wipePanel()
+        this.wipeMain()
+    }
+
+    static buildPanel (nodesArray) {
+        nodesArray.forEach( node => document.getElementById('left-container').innerHTML += node)
+    }
+
+    static buildMain (nodesArray) {
+        nodesArray.forEach( node => document.getElementById('game-container').innerHTML += node)
+    }
+
+    static buildPage (nodesObject) {
+        this.buildPanel(nodesObject.panel)
+        this.buildMain(nodesObject.main)
     }
 
 }
