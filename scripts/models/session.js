a81c0ec15e9448336e72ada3d0bfc040cfe433e1
Eli Cusic <tntitans31@gmail.com>
2020-12-12 16:37:06 -0600

    built out session class to handle creating instances

---
+++ scripts/models/session.js
@@ -0,0 +1,10 @@
+class Session {
+
+    constructor (userId, name) {
+        this.userId = userId
+        this.name = name
+        // this.display = new Display
+        this.game = {}
+    }
+    
+}
