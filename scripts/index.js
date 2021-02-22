4f6a566fc0d30b9bc2d6a324170880b4a7a5048c
Eli Cusic <tntitans31@gmail.com>
2021-02-22 13:35:22 -0600

    got rid of unnecessary files and updated checklist

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,3 @@
-import { practice } from "./practice.js"
 import { OpeningPage } from './pages/openingPage.js'
 
-practice()
-
 OpeningPage.init()
-
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-
-    // OpeningPage.init()
-
-// })
--- scripts/practice.js
+++
@@ -1,3 +0,0 @@
-export const practice = () => {
-    console.log("this is the practice")
-}
